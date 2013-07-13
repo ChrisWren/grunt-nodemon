56c753cb4c1c2fd5ab8430cd50bfbad411338498
ChrisWren <cthewren@gmail.com>
2013-07-13 15:38:15 -0700

    Use spec test reporter

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
         timeout: 3000,
         ignoreLeaks: false,
         ui: 'bdd',
-        reporter: 'nyan'
+        reporter: 'spec'
       },
       all: {
         src: ['test/**/*.js']
