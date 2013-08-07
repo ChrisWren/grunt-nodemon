6afa183f8ccdbafed0bfafecd69c6585efded0b8
ChrisWren <cthewren@gmail.com>
2013-08-06 20:28:05 -0700

    Added matchdep and jshint to grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   grunt.initConfig({
     nodemon: {
       all: {
@@ -13,12 +14,6 @@ module.exports = function(grunt) {
           env: {
             PORT: '8181'
           }
-        }
-      },
-      exec: {
-        options: {
-          file: 'test/test.txt',
-          exec: 'touch'
         }
       },
       cwd: {
@@ -41,10 +36,42 @@ module.exports = function(grunt) {
       all: {
         src: ['test/**/*.js']
       }
+    },
+    jshint: {
+      options: {
+        bitwise: true,
+        indent: 2,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        nonew: true,
+        quotmark: 'single',
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        trailing: true,
+        eqnull: true,
+        node: true,
+        expr: true,
+        evil: true,
+        globals: {
+          describe: true,
+          it: true,
+          before: true
+        }
+      },
+      files: {
+        src:  ['*.js', 'test/*.js', 'tasks/*.js']
+      }
     }
   });
+
   grunt.registerTask('default', ['nodemon']);
-  grunt.registerTask('test', ['simplemocha']);
-  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.registerTask('test', ['jshint', 'simplemocha']);
+
   grunt.loadTasks('tasks');
+
 };
